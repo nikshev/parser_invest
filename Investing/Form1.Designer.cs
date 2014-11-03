3be71a3c3857c8008a6c5655daea013c497fef4d
nikshev <nikshev81@gmail.com>
2014-11-03 13:32:25 +0200

    First and last commit :)

---
+++ Investing/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Investing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Invsetion";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder for save";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(355, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            this.saveFileDialog1.Title = "Save file ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(470, 241);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Log file";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Proxy";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(292, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Enable proxy";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(316, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 36);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "TEST";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 120000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 391);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Investion";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Investing/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Investing
+{
+    public partial class Form1 : Form
+    {
+        private bool inAdmin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Security.Principal.WindowsIdentity MyIdent = System.Security.Principal.WindowsIdentity.GetCurrent();
+
+            // Create a principal.
+            System.Security.Principal.WindowsPrincipal MyPrincipal = new System.Security.Principal.WindowsPrincipal(MyIdent);
+
+            // Check the role using a string.
+            if (MyPrincipal.IsInRole(@"BUILTIN\Administrators"))
+            {
+                richTextBox1.Text += "[" + DateTime.Now.ToString() + "] - You are an administrator. \r\n";
+                textBox1.Text = Application.StartupPath + "\\{{filename}}.csv";
+                // textBox2.Text = "192.168.0.1:3128";
+                //checkBox1.Checked = true;
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+                button2_Click(null, null);
+                inAdmin = true;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("You are not an administrator.");
+                inAdmin = false;
+            }
+        }
+
+        private void SavePrediction()
+        {
+            try
+            {
+                var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                string eurcad_url = "http://ru.investing.com/currencies/eur-cad-technical?period=86400";
+                string eurgbp_url = "http://ru.investing.com/currencies/eur-gbp-technical?period=86400";
+                string audusd_url = "http://ru.investing.com/currencies/aud-usd-technical?period=86400";
+                string audcad_url = "http://ru.investing.com/currencies/aud-cad-technical?period=86400";
+                string gbpusd_url = "http://ru.investing.com/currencies/gbp-usd-technical?period=86400";
+
+
+
+                string userAgentString = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; Tablet PC 2.0; .NET4.0C; .NET4.0E)";
+
+                System.Net.WebClient client = new System.Net.WebClient();
+
+                if (checkBox1.Checked && textBox2.Text != null)
+                {
+                    System.Net.WebProxy proxy = new System.Net.WebProxy(textBox2.Text);
+                    proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                    client.Proxy = proxy;
+                }
+
+                string DestStr = "";
+                string SymbStr = "";
+                for (int i = 0; i < 5; i++)
+                {
+                    if (i == 0)
+                    {
+                        string baseHtml = "";
+                        client.Headers.Add("user-agent", userAgentString);
+                        byte[] pageContent = client.DownloadData(eurcad_url);
+                        UTF8Encoding utf = new UTF8Encoding();
+                        baseHtml = utf.GetString(pageContent);
+                        htmlDocument.LoadHtml(baseHtml);
+                        SymbStr = "EURCAD;";
+                    }
+                    else if (i == 1)
+                    {
+                        string baseHtml = "";
+                        client.Headers.Add("user-agent", userAgentString);
+                        byte[] pageContent = client.DownloadData(eurgbp_url);
+                        UTF8Encoding utf = new UTF8Encoding();
+                        baseHtml = utf.GetString(pageContent);
+                        htmlDocument.LoadHtml(baseHtml);
+                        SymbStr = "EURGBP;";
+                    }
+                    else if (i == 2)
+                    {
+                        string baseHtml = "";
+                        client.Headers.Add("user-agent", userAgentString);
+                        byte[] pageContent = client.DownloadData(audusd_url);
+                        UTF8Encoding utf = new UTF8Encoding();
+                        baseHtml = utf.GetString(pageContent);
+                        htmlDocument.LoadHtml(baseHtml);
+                        SymbStr = "AUDUSD;";
+                    }
+                    else if (i == 3)
+                    {
+                        string baseHtml = "";
+                        client.Headers.Add("user-agent", userAgentString);
+                        byte[] pageContent = client.DownloadData(audcad_url);
+                        UTF8Encoding utf = new UTF8Encoding();
+                        baseHtml = utf.GetString(pageContent);
+                        htmlDocument.LoadHtml(baseHtml);
+                        SymbStr = "AUDCAD;";
+                    }
+                    else if (i == 4)
+                    {
+                        string baseHtml = "";
+                        client.Headers.Add("user-agent", userAgentString);
+                        byte[] pageContent = client.DownloadData(gbpusd_url);
+                        UTF8Encoding utf = new UTF8Encoding();
+                        baseHtml = utf.GetString(pageContent);
+                        htmlDocument.LoadHtml(baseHtml);
+                        SymbStr = "GBPUSD;";
+                    }
+
+                    HtmlNodeCollection nodes = htmlDocument.DocumentNode.SelectNodes("//span[@class='studySummaryOval buy arial_12 bold']");
+                    if (nodes == null)
+                        nodes = htmlDocument.DocumentNode.SelectNodes("//span[@class='studySummaryOval sell arial_12 bold']");
+                    if (nodes == null)
+                        nodes = htmlDocument.DocumentNode.SelectNodes("//span[@class='studySummaryOval neutral arial_12 bold']");
+
+
+                    if (nodes != null)
+                    {
+                        foreach (var Item in nodes)
+                        {
+                            if (Item != null)
+                            {
+                                if (Item.InnerText == "АКТИВНО ПОКУПАТЬ" || Item.InnerText == "ПОКУПАТЬ")
+                                    SymbStr += "1\r\n";
+                                else if (Item.InnerText == "АКТИВНО ПРОДАВАТЬ" || Item.InnerText == "ПРОДАВАТЬ")
+                                    SymbStr += "-1\r\n";
+                                else
+                                    SymbStr += "0\r\n";
+
+                                DestStr += SymbStr;
+                            }
+                        }
+                    }
+                }
+
+                richTextBox1.Text += "[" + DateTime.Now.ToString() + "] Prediction:\r\n";
+                richTextBox1.Text += DestStr;
+                DateTime dt_now = DateTime.Now;
+                DateTime dt = dt_now.AddHours(1);
+                string filename = dt.Day.ToString() + "_" + dt.Month.ToString() + "_" + dt.Year.ToString();
+                string Path = textBox1.Text;
+                string dest = ReplaceString(Path, "{{filename}}", filename, StringComparison.CurrentCulture);
+                System.IO.File.WriteAllText(@dest, DestStr);
+                richTextBox1.Text += "[" + DateTime.Now.ToString() + "] File:"+filename+".csv saved...\r\n";
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "[" + DateTime.Now.ToString() + "] Exception:" + ex.Message + "....\r\n";
+            }
+
+        }
+
+        public static string ReplaceString(string str, string oldValue, string newValue, StringComparison comparison)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int previousIndex = 0;
+            int index = str.IndexOf(oldValue, comparison);
+            while (index != -1)
+            {
+                sb.Append(str.Substring(previousIndex, index - previousIndex));
+                sb.Append(newValue);
+                index += oldValue.Length;
+
+                previousIndex = index;
+                index = str.IndexOf(oldValue, index, comparison);
+            }
+            sb.Append(str.Substring(previousIndex));
+
+            return sb.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (!inAdmin)
+                this.Close();
+
+            if (DateTime.Now.Hour == 23 && DateTime.Now.Minute == 40)
+            {
+                SavePrediction();
+                timer1.Enabled = false;
+                timer2.Enabled = true;
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+            this.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.ShowInTaskbar = false;
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "START")
+            {
+                if (textBox1.Text != "")
+                {
+                    richTextBox1.Text = richTextBox1.Text + "[" + DateTime.Now.ToString() + "] Investion started....\r\n";
+                    timer1.Enabled = true;
+                }
+                button2.Text = "STOP";
+            }
+            else if (button2.Text == "STOP")
+            {
+                richTextBox1.Text = richTextBox1.Text + "[" + DateTime.Now.ToString() + "] Investion stoped....\r\n";
+                timer1.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = Application.ExecutablePath;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = saveFileDialog1.FileName;
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                textBox2.ReadOnly = false;
+            else
+                textBox2.ReadOnly = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                SavePrediction();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+        }
+    }
+}
